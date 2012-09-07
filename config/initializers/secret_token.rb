d8230fa395a34e5a1be8beb17c17c46ef20c2fcf
Noah Stahl <n.stahl@me.com>
2012-09-07 15:37:54 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Electronic::Application.config.secret_token = '87af855994ab905b12a73411ef95f3302461d35c2529c298c0074f5dffecec40295b34389c976a2348bf333f23c0e7129c1a24fcdd7d1734cd44a4ad5d9eebef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Electronic::Application.config.session_store :cookie_store, key: '_Electronic_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Electronic::Application.config.session_store :active_record_store
